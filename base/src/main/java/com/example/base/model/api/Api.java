8efc70fe0e68374560e5403de8a7f58d3506897c
lsj <1916530129@qq.com>
2019-07-22 18:49:29 +0800

    2019年7月22日18:49:14 提交人:曲学超

--- base/src/main/java/com/example/base/model/api/Api.java
+++ base/src/main/java/com/example/base/model/api/Api.java
@@ -1,5 +1,12 @@
 package com.example.base.model.api;
 
+import com.example.base.model.bean.CircleBean;
+import com.example.base.model.bean.CircleTitleBean;
+import com.example.base.model.bean.CustomPhotoBean;
+import com.example.base.model.bean.DefaultPhotoBean;
+import com.example.base.model.bean.HealthDetailsBean;
+import com.example.base.model.bean.HistoryBean;
+import com.example.base.model.bean.InterrogationBean;
 import com.example.base.model.bean.LoginBean;
 import com.example.base.model.bean.MineBean;
 import com.example.base.model.bean.RegisterBean;
@@ -63,27 +70,6 @@ public interface Api {
     @GET("health/doctor/verify/v1/findDoctorById")
     Observable<MineBean> getMine();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     //查询科室列表
     //http://172.17.8.100/health/share/knowledgeBase/v1/findDepartment
     @GET("health/share/knowledgeBase/v1/findDepartment")
@@ -115,62 +101,10 @@ public interface Api {
     @GET("health/doctor/inquiry/verify/v1/findInquiryRecordList")
     Observable<InterrogationBean> interrogation();
 
-    //查询系统形象照
-    @GET("health/doctor/v1/findSystemImagePic")
-    Observable<DefaultPhotoBean> getDefaultPhoto();
-
-    //选择系统提供形象照
-    @FormUrlEncoded
-    @POST("health/doctor/verify/v1/chooseImagePic")
-    Observable<SetChooesBean> getChooseDefault(@Field("imagePic") String imagePic);
-
-    //上传形象照
-    @POST("health/doctor/verify/v1/uploadImagePic")
-    Observable<CustomPhotoBean> getCustomPhoto(@Body MultipartBody imagePic);
-
-    //根据医生id查询医生信息
-    @GET("health/doctor/verify/v1/findDoctorById")
-    Observable<MineBean> getMine();
-
-    /**
-     * 刘大大的接口
-     * @return
-     */
-
-    //查询科室列表
-    //http://172.17.8.100/health/share/knowledgeBase/v1/findDepartment
-    @GET("health/share/knowledgeBase/v1/findDepartment")
-    Observable<CircleTitleBean> circle_title();
-
-    //病友圈列表
-    //http://172.17.8.100/health/doctor/sickCircle/v1/findSickCircleList
-    @GET("health/doctor/sickCircle/v1/findSickCircleList")
-    Observable<CircleBean> circle(@Query("departmentId") int departmentId, @Query("page") int page,
-                                  @Query("count") int count);
-
-    //根据关键词查询病友圈
-    //http://172.17.8.100/health/doctor/sickCircle/v1/searchSickCircle
-    @GET("health/doctor/sickCircle/v1/searchSickCircle")
-    Observable<SreachBean> sreach(@Query("keyWord") String keyWord);
-
-    //查看并病友圈详情
-    //http：//172.17.8.100/health/doctor/sickCircle/v1/findSickCircleInfo
-    @GET("health/doctor/sickCircle/v1/findSickCircleInfo")
-    Observable<HealthDetailsBean> details(@Query("sickCircleId") int sickCircleId);
-
-    //发表评论
-    //http：//172.17.8.100/health/doctor/sickCircle/verify/v1/publishComment
-    @POST("health/doctor/sickCircle/verify/v1/publishComment")
-    Observable<StatusBean>  comment(@Query("sickCircleId")int sickCircleId, @Query("content")String content);
-
-    //查询医生的问诊记录列表
-    //http：//172.17.8.100/health/doctor/inquiry/verify/v1/findInquiryRecordList
-    @GET("health/doctor/inquiry/verify/v1/findInquiryRecordList")
-    Observable<InterrogationBean> interrogation();
 
     //查询医生的历史问诊记录列表
     //http://172.17.8.100/health/doctor/inquiry/verify/v1/findHistoryInquiryRecord
     @GET("health/doctor/inquiry/verify/v1/findHistoryInquiryRecord")
-    Observable<HistoryBean> history(@Query("page")int page,@Query("count")int count);
+    Observable<HistoryBean> history(@Query("page")int page, @Query("count")int count);
 
 }
--- base/src/main/java/com/example/base/presenter/MainPresenter.java
+++ base/src/main/java/com/example/base/presenter/MainPresenter.java
@@ -130,11 +130,6 @@ public class MainPresenter extends BasePresenter<IMainView.IMainShow> {
                 });
     }
 
-
-
-
-
-
     //查询科室列表
     public void LoadShowCricleTitleData() {
         okHttp.api.circle_title()
@@ -234,145 +229,14 @@ public class MainPresenter extends BasePresenter<IMainView.IMainShow> {
 
 
 
-    //查询系统形象照
-    public void DefaultPhotoP() {
-        okHttp.api.getDefaultPhoto()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<DefaultPhotoBean>() {
-                    @Override
-                    public void accept(DefaultPhotoBean defaultPhotoBean) throws Exception {
-                        getView().onSuccess(defaultPhotoBean);
-                    }
-                });
-    }
-
-    //选择系统提供形象照
-    public void setChooseImageP(String imagePic) {
-        okHttp.api.getChooseDefault(imagePic)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<SetChooesBean>() {
-                    @Override
-                    public void accept(SetChooesBean setChooesBean) throws Exception {
-                        getView().onSuccess(setChooesBean);
-                    }
-                });
-    }
-
-    //根据医生id查询医生信息
-    public void MineP() {
-        okHttp.api.getMine()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<MineBean>() {
-                    @Override
-                    public void accept(MineBean mineBean) throws Exception {
-                        getView().onSuccess(mineBean);
-                    }
-                });
-    }
 
 
 
 
 
 
-    //查询科室列表
-    public void LoadShowCricleTitleData() {
-        okHttp.api.circle_title()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<CircleTitleBean>() {
-                    @Override
-                    public void accept(CircleTitleBean bean) throws Exception {
-                        getView().onSuccess(bean);
-                    }
-                });
-    }
-
-    //病友圈列表
-    public void LoadShowCircleData(int departmentId, int page, int count) {
-        okHttp.api.circle(departmentId, page, count)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<CircleBean>() {
-                    @Override
-                    public void accept(CircleBean bean) throws Exception {
-                        getView().onSuccess(bean);
-                    }
-                });
-    }
 
 
-    //搜索病友圈列表
-    public void LoadShowSreachData(String keyWord) {
-        okHttp.api.sreach(keyWord)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<SreachBean>() {
-                    @Override
-                    public void accept(SreachBean bean) throws Exception {
-                        getView().onSuccess(bean);
-                    }
-                });
-    }
-
-    //病友圈详情列表
-    public void LoadShowDetailsData(int sickCircleId) {
-        okHttp.api.details(sickCircleId)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<HealthDetailsBean>() {
-                    @Override
-                    public void accept(HealthDetailsBean bean) throws Exception {
-                        getView().onSuccess(bean);
-                    }
-                });
-    }
-
-    //上传形象照
-    public void CustomPhotoP(String imagePic) {
-        File file = new File(imagePic);
-        MultipartBody.Builder builder = new MultipartBody.Builder().setType(MultipartBody.FORM);
-        builder.addFormDataPart("imagePic", file.getName(), RequestBody.create(MediaType.parse("multipart/octet-stream"), file));
-        Observable<CustomPhotoBean> img1 = okHttp.api.getCustomPhoto(builder.build());
-
-        img1.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<CustomPhotoBean>() {
-                    @Override
-                    public void accept(CustomPhotoBean customPhotoBean) throws Exception {
-                        getView().onSuccess(customPhotoBean);
-                    }
-                });
-    }
-
-    //发表病友圈评论
-    public void LoadShowCommentData(int sickCircleId, String content) {
-        okHttp.api.comment(sickCircleId, content)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<StatusBean>() {
-                    @Override
-                    public void accept(StatusBean bean) throws Exception {
-                        getView().onSuccess(bean);
-                    }
-                });
-    }
-
-    //查询医生的问诊记录列表
-    public void LoadShowInterrogationBeanData() {
-        okHttp.api.interrogation()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<InterrogationBean>() {
-                    @Override
-                    public void accept(InterrogationBean bean) throws Exception {
-                        getView().onSuccess(bean);
-                    }
-                });
-    }
 
 
     //查询医生的历史问诊记录列表
